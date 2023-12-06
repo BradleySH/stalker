e6db802bef1607555cf11a156e4a082be51876d1
Bradley Horlander <78397249+BradleySH@users.noreply.github.com>
2023-12-05 19:23:34 -0600

    initial commit

---
+++ cmd/stalker/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/BradleySH/stalker/internal/routes"
+)
+
+func main() {
+	router := routes.NewRouter()
+
+	port := ":8080"
+	addr := fmt.Sprintf("Listening on %s\n", port)
+	fmt.Println(addr)
+
+	err := http.ListenAndServe(port, router)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BradleySH/stalker
+
+go 1.20
